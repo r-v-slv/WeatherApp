46d8a61f984174b99e3cc5b99a3561521df44028
slv <slv.4.dev@gmail.com>
2024-07-22 01:39:41 +0300

    1. Perform Request 2. Parse JSON 3. Fetch city

---
+++ WeatherApp/Model/WeatherDataModel.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherDataModel.swift
+//  WeatherApp
+//
+//  Created by SLV on 22.07.2024.
+//
+
+import Foundation
---
+++ WeatherApp/Model/WeatherManager.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherManager.swift
+//  WeatherApp
+//
+//  Created by SLV on 21.07.2024.
+//
+
+import Foundation
